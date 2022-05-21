7f06372d83f0cf8977d87d1cd15ee6a31c5d39e5
UrTaxiMom <92867660+UrTaxiMom@users.noreply.github.com>
2022-05-22 04:30:57 +0800

    Add files via upload

--- show.py
+++ show.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-#
+class ShowOnMonitor:
+    def __init__(self,text,img):
+        self.text=text
+        self.img = img
+    def show(self):
+        window = tk.Tk()
+        window.title('window')
+        window.geometry('800x600')
+        lbl_1 = tk.Label(window, text=A, bg='yellow', fg='#263238', font=('Arial', 24))
+        lbl_1.grid(column=10, row=0)
+        imgTk=ImageTk.PhotoImage(img)
+        lb=tk.Label(window,image=imgTk)
+        lb.image = imgTk
+        lb.grid(column=10,row=100)
+        window.mainloop()
 A="幹你娘機掰"
-window = tk.Tk()
-window.title('window')
-window.geometry('800x600')
-lbl_1 = tk.Label(window, text=A, bg='yellow', fg='#263238', font=('Arial', 24))
-lbl_1.grid(column=10, row=0)
-#
 img = Image.open('IMG_4338.jpg')
 img = img.resize((img.width//5,img.height//5))
-imgTk=ImageTk.PhotoImage(img)
-lb=tk.Label(window,image=imgTk)
-lb.image = imgTk
-lb.grid(column=10,row=100)
-#
-window.mainloop()
+warn=ShowOnMonitor(A,img)
+warn.show()
