80acce06d58a088c708f6ba0e63c5f4404bbb176
gha890826 <allenho324@gmail.com>
2022-05-23 18:23:49 +0800

    speaker modular, but some errors.

--- speaker.py
+++ speaker.py
@@ -1,38 +1,51 @@
 import os
 import time
 import playsound # please use playsound==1.2.2 to avoid unexpected error
-from gtts import gTTS
+from gtts import gTTS # langulage code list: https://developers.google.com/admin-sdk/directory/v1/languages
+    
+class Speaker():
+    """docstring for speaker. Speaker is for speaking waring."""
+    __texts={"en":"please wear on your mask","zh-tw":"請戴上口罩","ja":"マスクをする"}
+    """def __init__(self, arg):
+        super(speaker, self).__init__()
+        self.arg = arg"""
 
-# langulage code list: https://developers.google.com/admin-sdk/directory/v1/languages
+    def show_setting_GUI(self):
+        end
 
-def speak(mytext, mylang="en"):
-    try:
-        print('make '+mylang+': '+mytext)
-        time.sleep(1)
-        tts = gTTS(text=mytext, tld="com", lang=mylang)
-        filename = mylang+"_alarm_voice.mp3"
-        tts.save(filename)
-        play(filename)
-        os.remove(filename)
-    except:
-        print('error when make '+mylang+': '+mytext)
+    def speak_warning(self):
+        for text in __texts.keys():
+            __speak(text,__texts.get(text))
 
-def play(filename):
-    try:
-        print('play '+filename)
-        playsound.playsound('./'+filename)
-    except:
-        print('error when play '+filename)
+    def test(self):
+        print("start test")
+        self.__speak("this is speaker test",'en')
+        self.__speak("這是語音測試",'zh-tw')
+        self.__speak("これは音声テストです",'ja')
 
-mytext="please wear on your mask"
-language='en'
-speak(mytext,language)
-mytext="請戴上口罩"
-language='zh-tw'
-speak(mytext,language)
-mytext="マスクを着用してください"
-language='ja'
-speak(mytext,language)
+    def __speak(self,mytext, mylang="en"):
+        try:
+            print('make '+mylang+': '+mytext)
+            time.sleep(1)
+            tts = gTTS(text=mytext, tld="com", lang=mylang)
+            filename = mylang+"_alarm_voice.mp3"
+            tts.save(filename)
+            __play(filename)
+            os.remove(filename)
+        except:
+            print('error when make '+mylang+': '+mytext)
 
+    def __play(self,filename):
+        try:
+            print('play '+filename)
+            playsound.playsound('./'+filename)
+        except:
+            print('error when play '+filename)
+
+'''
+speaker =Speaker()
+print("start")
+speaker.test()
+'''
 # according https://stackoverflow.com/a/64367776/17732660
 # gTTS of gtts doc https://gtts.readthedocs.io/en/latest/module.html#gtts.tts.gTTS
