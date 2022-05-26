27d0218f9b7f31dd47ae83a77deee8e73a60afcb
NightHao <48900334+NightHao@users.noreply.github.com>
2022-05-27 00:44:25 +0800

    Add files via upload

--- mask_detection/main.py
+++ mask_detection/main.py
@@ -1,29 +1,33 @@
 import argparse
-from multiprocessing import parent_process
 import detect_mask as mask
 import show
-import time
-#import speaker 
+import speaker 
 import tkinter as tk
 class DetectMain:
     def __init__(self):
         self.image_model=mask.DetectImage()
         self.camera_model=mask.DetectCamera()
         self.screen = show.ShowOnMonitor() 
+        self.audio = speaker.Speaker()
         self.create_window()
         
-    def now_start(self, window):
+    def execute(self, window):
         window.destroy()
         self.detect()
+        self.create_window()
+        
+    def setting(self,window):
+        window.destroy()
+        self.screen.setting()
         self.create_window()
     
     def create_window(self):
         window = tk.Tk()
         window.title('window')
         window.geometry('1280x720')
-        detect_button = tk.Button(window, text='開始偵測', command = lambda: self.now_start(window))
+        detect_button = tk.Button(window, text='開始偵測', command = lambda: self.execute(window))
         detect_button.grid()
-        setting_button = tk.Button(window, text='設定')
+        setting_button = tk.Button(window, text='設定',command = lambda: self.setting(window))
         setting_button.grid()
         window.mainloop()
         
@@ -38,11 +42,11 @@ class DetectMain:
         Input="test.jpg"
         if(Input=="cam"):
             res, res_image = self.camera_model.image_processing()
-            #self.audio.speak_warning(res)
+            self.audio.speak_warning(res)
             self.screen.show(res, res_image)
         else:
             self.image_model.opt(Input)
             res, res_image=self.image_model.image_processing()
-            #self.audio.speak_warning(res)
+            self.audio.speak_warning(res)
             self.screen.show(res, res_image)
 detector=DetectMain()
--- mask_detection/show.py
+++ mask_detection/show.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+import time
 from PIL import Image, ImageTk
 class ShowOnMonitor:
     def __init__(self):
@@ -8,34 +10,69 @@ class ShowOnMonitor:
         self.label_bg=['green','red','yellow']   #字體背景顏色
         self.fg='#263238'   #字體顏色
         self.font=('Arial', 24)   #字體以及大小
-        self.window = tk.Tk()
-        self.window.title('window')
-        self.window.geometry('1280x720')
+        #self.window = tk.Tk()
+        #self.window.title('window')
+        #self.window.geometry('1280x720')
         #self.mybutton = tk.Button(self.window, text='button', command= lambda img=self.img,result=0:self.show(result,img))
         #self.mybutton.grid(column=200,row=200)
         #self.window.mainloop()
+    def choose_bg(self):
+        self.modify_bg(self.bg_color.get())
+        
+    def choose_fg(self):
+        self.modify_fg(self.fg_color.get())
+        
     def modify_text(self,new_text):
         self.text=new_text
+        
     def modify_bg(self,new_bg):
         self.bg=new_bg
+        
     def modify_label_bg(self,new_label_bg):
         self.label_bg=new_label_bg
+        
     def modify_fg(self,new_fg):
         self.fg=new_fg
+        
     def modify_font(self,new_font):
         self.font=new_font
+        
+    def setting(self):
+        window = tk.Tk()
+        window.title('window')
+        window.geometry('1280x720')
+        window.configure(bg=self.bg)
+        bg_colors = ['purple','green','orange','yellow','red','white']
+        self.bg_color = ttk.Combobox(window, state='readonly')
+        self.bg_color['values'] = bg_colors
+        self.bg_color.grid(column=0,row=0)
+        self.bg_color.current(0)
+        self.a=tk.Button(window, text='change background', command= self.choose_bg)
+        self.a.grid(column=0,row=1)
+        fg_colors = ['blue','green','orange','yellow','red','white']
+        self.fg_color = ttk.Combobox(window, state='readonly')
+        self.fg_color['values'] = fg_colors
+        self.fg_color.grid(column=0,row=2)
+        self.fg_color.current(0)
+        self.e=tk.Button(window, text='change font color', command= self.choose_fg)
+        self.e.grid(column=0,row=4)
+        window.mainloop()
+        
     def show(self,result,img):
         #self.mybutton.destroy()
-        self.window.configure(bg=self.bg)
-        lbl_1 = tk.Label(self.window, text=self.text[result], bg=self.label_bg[result], fg=self.fg, font=self.font)
-        lbl_1.grid(column=10, row=0)
+        window = tk.Tk()
+        window.title('window')
+        window.geometry('1280x720')
+        window.configure(bg=self.bg)
+        lbl_1 = tk.Label(window, text=self.text[result], bg=self.label_bg[result], fg=self.fg, font=self.font)
+        lbl_1.grid(column=0, row=2)
         self.img=img
         self.img = self.img.resize((self.img.width*2,self.img.height*2))
         imgtk=ImageTk.PhotoImage(self.img)
-        lb=tk.Label(self.window,image=imgtk)
+        lb=tk.Label(window,image=imgtk)
         lb.image = imgtk
-        lb.grid(column=10,row=100)
-        self.window.mainloop()
+        lb.grid(column=0,row=3)
+        window.mainloop()
 #img = Image.open('IMG_4338.jpg')
 #warn=ShowOnMonitor()
 #warn.show(0,img)
